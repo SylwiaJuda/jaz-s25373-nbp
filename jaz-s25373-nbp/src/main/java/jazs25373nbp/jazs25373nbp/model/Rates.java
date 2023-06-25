6e2190a907f0a61f6197cc1a4ded3141847a8080
SylwiaJuda <93827662+SylwiaJuda@users.noreply.github.com>
2023-06-25 14:51:54 +0200

    Update Rates.java

--- jaz-s25373-nbp/src/main/java/jazs25373nbp/jazs25373nbp/model/Rates.java
+++ jaz-s25373-nbp/src/main/java/jazs25373nbp/jazs25373nbp/model/Rates.java
@@ -1,52 +1,50 @@
-package jazs25373nbp.jazs25373nbp.model;
+package nbp.nbp.nbpmodel;
 
 public class Rates {
+    private String no;
+    private String effectiveDate;
+    private Double mid;
 
-        private String no;
-        private String effectiveDate;
-        private Double mid;
+    public Rates() {
 
-        public Rates() {
+    }
 
-        }
+    public Rates(String no, String effectiveDate, Double mid) {
+        this.no = no;
+        this.effectiveDate = effectiveDate;
+        this.mid = mid;
+    }
 
-        public Rates(String no, String effectiveDate, Double mid) {
-            this.no = no;
-            this.effectiveDate = effectiveDate;
-            this.mid = mid;
-        }
+    public String getNo() {
+        return no;
+    }
 
-        public String getNo() {
-            return no;
-        }
+    public void setNo(String no) {
+        this.no = no;
+    }
 
-        public void setNo(String no) {
-            this.no = no;
-        }
+    public String getEffectiveDate() {
+        return effectiveDate;
+    }
 
-        public String getEffectiveDate() {
-            return effectiveDate;
-        }
+    public void setEffectiveDate(String effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
 
-        public void setEffectiveDate(String effectiveDate) {
-            this.effectiveDate = effectiveDate;
-        }
+    public Double getMid() {
+        return mid;
+    }
 
-        public Double getMid() {
-            return mid;
-        }
+    public void setMid(Double mid) {
+        this.mid = mid;
+    }
 
-        public void setMid(Double mid) {
-            this.mid = mid;
-        }
-
-        @Override
-        public String toString() {
-            return "Rates{" +
-                    "no='" + no + '\'' +
-                    ", effectiveDate='" + effectiveDate + '\'' +
-                    ", mid=" + mid +
-                    '}';
-        }
-
+    @Override
+    public String toString() {
+        return "Rates{" +
+                "no='" + no + '\'' +
+                ", effectiveDate='" + effectiveDate + '\'' +
+                ", mid=" + mid +
+                '}';
+    }
 }
