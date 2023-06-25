8108a2703a71c2faeb5fde8bc25b03db248ec991
SylwiaJuda <93827662+SylwiaJuda@users.noreply.github.com>
2023-06-25 14:32:35 +0200

    Add files via upload
    Początkowa wersja wszystkich plików

---
+++ jaz-s25373-nbp/src/main/java/jazs25373nbp/jazs25373nbp/JazS25373NbpApplication.java
@@ -0,0 +1,13 @@
+package jazs25373nbp.jazs25373nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS25373NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS25373NbpApplication.class, args);
+	}
+
+}
---
+++ jaz-s25373-nbp/src/test/java/jazs25373nbp/jazs25373nbp/JazS25373NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package jazs25373nbp.jazs25373nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS25373NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
